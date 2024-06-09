8f539494db1d19f9ae38939f406376cf7de28537
Gohil Jayrajsinh N <137603167+Gohil-Jayrajsinh@users.noreply.github.com>
2024-06-09 08:49:59 +0530

    Delete Machine_Learning_series/pro_1_car_prediction directory

--- Machine_Learning_series/pro_1_car_prediction/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
- doing a Creoss validation hear
-# try:
-#     cro_vali_scor = cross_val_score(my_rand_fore, x_test, y_test, scoring="neg_mean_squared_error", cv=10)
-#     rmse_scores = np.sqrt(-cro_vali_scor)
-#     print("rmse scores : ", rmse_scores)
-#     print("rmse scores mean : ", rmse_scores.mean())
-#     print("rmse scores std : ", rmse_scores.std())
-# except Exception as e:
-#     print(e)
